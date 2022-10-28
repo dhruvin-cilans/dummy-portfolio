08a07ac7161589fcbfe2b24b6d10289f07d9cd5f
jgojariya@sigmasolve.net <dhruvinhprajapati@gmail.com>
2022-10-28 11:15:26 +0530

    feat: home page changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,18 +3,37 @@ import { UserData } from '../../fixtures/user.mock'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
+        <div style={{
+            textAlign: "center",
+            marginTop: "-32px",
+            height: "100vh",
+            backgroundColor: 'darksalmon',
+            color: "CaptionText",
+            fontSize: "1.5rem",
+        }}>
+            <h1 style={{
+                fontSize: '3rem',
+                fontWeight: 'bold',
+                backgroundColor: '#eee',
+            }}>Portfolio</h1>
+            <img src={UserData.avatar} alt="profile"></img>
             <p>{UserData.name}</p>
             <p>{UserData.email}</p>
             <p>{UserData.contact}</p>
             <p>{UserData.about}</p>
-            <p>{UserData.socials.instagram}</p>
-            <p>{UserData.socials.facebook}</p>
-            <p>{UserData.socials.linkedin}</p>
-            <p>{UserData.socials.twitter}</p>
+            <a href={UserData.socials.instagram}>
+                <img src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png" alt="instagram" />
+            </a>
+            <a href={UserData.socials.facebook}>
+                <img src="https://img.icons8.com/ios-filled/50/000000/facebook-new.png" alt="facebook" />
+            </a>
+            <a href={UserData.socials.linkedin}>
+                <img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="linkedin" />
+            </a>
+            <a href={UserData.socials.twitter}>
+                <img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="twitter" />
+            </a>
         </div>
-
     )
 }
 

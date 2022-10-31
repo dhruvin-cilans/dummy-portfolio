6a1864a950a8270ec4e758269a931fab63eed07e
dhruvi <dhruvi.cilans@gmail.com>
2022-10-31 16:54:20 +0530

    fix: user.mock.js userData

--- src/fixtures/user.mock.js
+++ src/fixtures/user.mock.js
@@ -1,56 +1,29 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material/';
+
 export const UserData = {
     id: 1,
-    name: 'Dhruvi H. Mehta',
-    email: 'dhruvi@gmail.com',
-    contact: '',
-    about: "DevOps & Data Science Engineer | AWS Certified Solutions Architect",
-    avatar: '',
-    cover: '',
+    name: 'John Doe',
+    email: 'johm@doe.com',
+    contact: '+1 234 567 890',
+    about: "Software Engineer",
+    avatar: 'https://i.pravatar.cc/150?img=1',
+    cover: " https://images.unsplash.com/photo-1517436073-3b1b1b1b1b1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFja2dyb3VuZCUyMGNvbnRyb2wlMjBjb2xvciUyMGRlZmF1bHR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
     socials: {
-        facebook: '',
-        twitter: '',
-        linkedin: '',
-        instagram: '',
+        facebook: {
+            url: 'https://www.facebook.com/',
+            logo: <Facebook />
+        },
+        instagram: {
+            url: 'https://www.instagram.com/',
+            logo: <Instagram />
+        },
+        linkedin: {
+            url: 'https://www.linkedin.com/',
+            logo: <LinkedIn />
+        },
+        twitter: {
+            url: 'https://twitter.com/',
+            logo: <Twitter />
+        }
     },
-    posts: [
-        {
-            id: 1,
-            title: 'Post 1',
-            content: 'Post 1 content',
-            image: '',
-            likes: 0,
-            comments: 0,
-            shares: 0,
-            createdAt: '2020-01-01',
-            updatedAt: '2020-01-01',
-        },
-        {
-            id: 2,
-            title: 'Post 2',
-            content: 'Post 2 content',
-            image: '',
-            likes: 0,
-            comments: 0,
-            shares: 0,
-            createdAt: '2020-01-01',
-            updatedAt: '2020-01-01',
-        },
-    ],
-    portfolio: [
-        {
-            id: 1,
-            title: 'Portfolio 1',
-            content: 'Portfolio 1 content',
-            image: '',
-            link: '',
-        },
-        {
-            id: 2,
-            title: 'Portfolio 2',
-            content: 'Portfolio 2 content',
-            image: '',
-            link: '',
-        },
-    ],
-
 }
